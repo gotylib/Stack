1dd2d1f0f4b116c7759df8181c0c37dc153aaf9d
gotylib <116037061+gotylib@users.noreply.github.com>
2023-10-02 12:29:15 +0300

    Update ООП лаба 2.cpp

--- ООП лаба 2.cpp
+++ ООП лаба 2.cpp
@@ -3,25 +3,97 @@ private:
 	double* real;
 	double* imag;
 public:
-	Complex(double real, double imag);
+	//Complex();
+	/// <summary>
+	/// конструктор с параметрами
+	/// </summary>
+	/// <param name="real">действительная часть</param>
+	/// <param name="imag">мнимая</param>
+	Complex(double real = 0, double imag = 0);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="copy">копируемое</param>
 	Complex(const Complex& copy);
 	~Complex();
 	double* getReal() const { return real; }
 	double* getImag() const { return imag; }
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="real">реальная часть</param>
 	void setReal(double& real) { *(this->real) = real; }
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="imag">мнимая часть</param>
 	void setImag(double& imag) { *(this->imag) = imag; }
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="conclusion">поток вывода</param>
+	/// <param name="out">выводимое</param>
+	/// <returns>поток ввода</returns>
 	friend std::ostream& operator <<(std::ostream& conclusion, Complex& out);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="input">поток ввода</param>
+	/// <param name="in">вводимое</param>
+	/// <returns>поток вывода</returns>
 	friend std::istream& operator >>(std::istream& input, Complex& in);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="summ">слогаемое</param>
+	/// <returns>сумма</returns>
 	Complex operator +(const Complex& summ);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="subtraction">вычитаемое</param>
+	/// <returns>разность</returns>
 	Complex operator -(const Complex& subtraction);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="div">делитель</param>
+	/// <returns></returns>
 	Complex operator /(const Complex& div);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="mult">множитель</param>
+	/// <returns></returns>
 	Complex operator *(const Complex& mult);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="equally">приравнимое</param>
+	/// <returns>деление</returns>
 	Complex& operator =(const Complex& equally);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="comp">Сравнимое</param>
+	/// <returns>умножение</returns>
 	bool operator >(const Complex& comp);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="comp">сравнимое</param>
+	/// <returns></returns>
 	bool operator <(const Complex& comp);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="comp">сравнимое</param>
+	/// <returns></returns>
 	bool operator ==(const Complex& comp);
 
 };
+
+#include"Complex.h"
 
 Complex::Complex(double real, double imag) {
 	this->real = new double;
@@ -57,11 +129,11 @@ std::istream& operator >>(std::istream& input, Complex& in) {
 }
 
 Complex Complex::operator+(const Complex& summ) {
-	return Complex (*real + *(summ.real), *imag + *(summ.imag));
+	return Complex(*real + *(summ.real), *imag + *(summ.imag));
 }
 
 Complex Complex::operator-(const Complex& summ) {
-	 return  Complex (*real - *(summ.real), *imag - *(summ.imag));
+	return  Complex(*real - *(summ.real), *imag - *(summ.imag));
 }
 
 Complex Complex::operator*(const Complex& mult) {
@@ -77,9 +149,9 @@ Complex& Complex::operator=(const Complex& equally) {
 		return *this;
 	}
 	else {
-		delete real; 
+		delete real;
 		delete imag;
-		real = new double; 
+		real = new double;
 		imag = new double;
 		*real = *(equally.real);
 		*imag = *(equally.imag);
